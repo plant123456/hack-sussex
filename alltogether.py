a04bef356e58e6418c8151745661dc7b690fb305
Shanghangchua <chuashanghang02@gmail.com>
2023-02-19 05:26:08 +0000

    naive_bayes_yassssssss

---
+++ alltogether.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Overall
+"""
+import Chatbox
+import naive_bayes_model
+import naive_bayes_high_risk
+#import #chatbot created
+
+create pandas dataframe
+create an instance of Chatbox
+
+in first textbox
+(
+ Create an instance of chatbot
+ get the chatbot to interact with the user 
+ - give appropriate response
+ if no response:
+ call naive bayes model using the chatbox stored response as input
+ naive bayes model outputs group 
+- give appropriate response based on group
+ else if response is high risk:
+     call naive_bayes_high risk
+     add text to dataframe
+     add user name to dataframe
+     add classification to dataframe
+ )  
+
+in second textbox:
+    show pandas dataframe
+    
