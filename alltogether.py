0f28ea88a26739a53ba66fedc9e683fcec5e3882
Shanghangchua <chuashanghang02@gmail.com>
2023-02-19 10:10:46 +0000

    update

--- alltogether.py
+++ alltogether.py
@@ -5,14 +5,19 @@ Overall
 from tkinter import *
 import pandas as pd
 import tkinter.ttk as ttk
-import nb_hr as predict
-import chatbot as chat
+import nb_hr as predict_hr
+import nb_general as predict_general
+from chatbot import Chatbot
+from random import *
 
 #import #chatbot created
 
 #create pandas dataframe
 data = {'user_id': [], 'problem statement': [], 'problem category': []}
 data_to_show = pd.DataFrame(data)
+
+#create chatbot
+bot = Chatbot()
 
 #create an instance of Chatbox
 root = Tk()
@@ -27,15 +32,24 @@ chat_history.place(x=6, y=6, height=385, width=780)
 # Create a Text widget to type in new messages
 message_input = Text(root, bd=0, bg="white", width="66", height="4", font=("Helvetica", 18), foreground="#000000")
 message_input.place(x=6, y=400, height=88, width=780)
+#list of user messages
+message_list = []
 
+sent_message = False
 # Create a button to send the message
 def send_message():
+    global sent_message
     message_text = message_input.get("1.0", END).strip()
     if message_text:
         chat_history.config(state=NORMAL)
         chat_history.insert(END, "You: " + message_text + "\n")
         chat_history.config(state=DISABLED)
+        message_list.append(str(message_text))
+        print(message_list)
         message_input.delete("1.0", END)
+        sent_message = True
+        print(sent_message)
+    
 
 send_button = Button(message_input, text="Send", width="10", height=3,
                      bd=0, bg="#4b0082", activebackground="#000000", foreground='white', font=("Helvetica", 18),
@@ -60,11 +74,53 @@ tree.configure(yscroll=scrollbar.set)
 scrollbar.pack(side='right', fill='y')
 tree.pack(side='left', fill='both', expand=True)
 
-# Add some sample chat history
-chat_history.insert(END, "Chat Bot: Hi, how can I assist you today?\n")
-chat_history.insert(END, "User: Can you recommend a good restaurant?\n")
-chat_history.insert(END, "Chat Bot: Yes, there's a great Italian restaurant on Main Street.\n")
-chat_history.insert(END, "User: Thanks, I'll check it out!\n")
+#inserts initial message
+chat_history.insert(END, f"Chat Bot: {bot.intro} \n")
+responses = ['Is your Storage Heater, Boiler, Insulation or Heat Pump not working properly? You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue. If the issue persist you can visit this website https://www.edfenergy.com/heating/boilers/new-boiler.',
+             "Do you have an issue with supply disruption or need a new connection? You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue.",
+             "Do you have any issue in MyAccount or need a new connection? You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue.",
+             "Do you have any issue with Payments, Refunds, Energy Bills or want to know about Support Scheme?  You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue.",
+             "Do you have any issue with Paying your bills or want to know to reduce energy costs/Maximize household income ?  You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue."]
+#main loop
+if (sent_message == True):
+    print(sent_message)
+    response_1 = bot.chat(message_list[0])
+
+    if len(response_1) > 0:
+        chat_history.insert(END, response_1)
+    else:
+        general_category = predict_general.naive_bayes(message_list)
+        if general_category == 'boiler heater':
+            chat_history.insert(END, f'Based on the topic of your problem here is the following advice {responses[0]}')
+        elif general_category == 'thermostat':
+            chat_history.insert(END, f'Based on the topic of your problem here is the following advice {responses[0]}')
+        elif general_category == 'supply':
+            chat_history.insert(END, f'Based on the topic of your problem here is the following advice {responses[1]}')
+        elif general_category == 'meter':
+            chat_history.insert(END, f'Based on the topic of your problem here is the following advice {responses[1]}')
+        elif general_category == 'account':
+            chat_history.insert(END, f'Based on the topic of your problem here is the following advice {responses[2]}')
+        elif general_category == 'payment':
+            chat_history.insert(END, f'Based on the topic of your problem here is the following advice {responses[3]}')
+        elif general_category == 'struggling to pay':
+            chat_history.insert(END, f'Based on the topic of your problem here is the following advice {responses[4]}')
+        elif general_category == 'high risk':
+            hr_category = predict_hr.naive_bayes_hr(message_list)
+            if hr_category == 'family': 
+                data_to_show['user id'].append(randint())
+                data_to_show['problem statement'] = message_list[0]
+                data_to_show['problem category'] = hr_category
+                chat_history.insert(END, 'Your problem has identified you as a Priority Risk customer')
+            elif hr_category == 'special_condition':
+                data_to_show['user id'].append(randint())
+                data_to_show['problem statement'] = message_list[0]
+                data_to_show['problem category'] = hr_category
+                chat_history.insert(END, 'Your problem has identified you as a Priority Risk customer')
+            elif hr_category == 'medicine':
+                data_to_show['user id'].append(randint())
+                data_to_show['problem statement'] = message_list[0]
+                data_to_show['problem category'] = hr_category
+                chat_history.insert(END, 'Your problem has identified you as a Priority Risk customer')
 
 # Disable editing in the chat history widget
 chat_history.config(state=DISABLED)
@@ -81,27 +137,9 @@ root.config(menu=main_menu)
 
 scrollbar = Scrollbar(root, command=chat_history.yview, cursor="star")
 scrollbar.place(x=788, y=5, height=385)
-
-root.mainloop()
 #End of chatbox creation
 
+#words
 
-'''in first textbox
-(
- Create an instance of chatbot
- get the chatbot to interact with the user 
- - give appropriate response
- if no response:
- call naive bayes model using the chatbox stored response as input
- naive bayes model outputs group 
-- give appropriate response based on group
- else if response is high risk:
-     call naive_bayes_high risk
-     add text to dataframe
-     add user name to dataframe
-     add classification to dataframe
- )  
 
-in second textbox:
-    show pandas dataframe'''
-    
+root.mainloop()
--- chatbot.py
+++ chatbot.py
@@ -1,57 +1,57 @@
 import random  #prefinal code
 import re
 flag=0
-def chat():
-  while True:
-    print("Hello, I'm a chatbot. What's your name?")
-    name = input("Your name: ")
-    print("Nice to meet you",name,)
-    print("How can I help you?")
-    usermessage = input("You: ")
-    words1=["boiler","storage heater","insulation","heat pump","contact someone on my boiler","contact someone on my storage heater","contact someone on my insulation","contact someone on my heat pump"]
-    words2=["faulty thermostat", "damaged thermostat", "display thermostat","HeatSmart", "Netamo smart thermostat"]
-    words3=["electricity supply","gas supply","new connection","new supply","supply disruption","electricity disruption","gas disruption"]
-    words4=["MyAccount", "personal details", "account details", "edf app"]
-    words5=["payment", "payment due","recent payment", "refund", "Energy Bills", "Support Scheme", "Help with the Energy Bills Support Scheme"]
-    words6=["Reduce energy costs", "energy costs", "Maximise household income", "household income", "Paying my bills", "Paying bills", "General debt advice", "debt"]
-    words7=["Illness","disability","mental health issue"]
-    words8=["child", "live with a child", "children", "live alone", "reached state pension age", "live alone or with others and have reached state pension age","disabled",
-    "chronically ill state", "pension age or are disabled","pension age or are disabled", "pensionable age or are disabled","chronically ill or under 18 years old", 
-    "under 18 years old", "medicine refrigerator problem", "medicine refrigerator", "elderly have problem reading their bills","elder", "elderly have problem with hearing on the phone",
-    "elderly have problem with using electronic payments"," child under five years old"]
-    if re.compile('|'.join(words1),re.IGNORECASE).search(usermessage):
-      print("Is your Storage Heater, Boiler, Insulation or Heat Pump not working properly? You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue. If the issue persist you can visit this website https://www.edfenergy.com/heating/boilers/new-boiler.")
-      flag=0
-      break
-    elif re.compile('|'.join(words2),re.IGNORECASE).search(usermessage):
-      print("Is your Smart in-home device, in-home display thermostat or HeatSmart/Netamo smart thermostat Boiler not working properly? You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue.")
-      flag=0
-      break
-    elif re.compile('|'.join(words3),re.IGNORECASE).search(usermessage):
-      print("Do you have an issue with supply disruption or need a new connection? You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue.")
-      flag=0
-      break
-    elif re.compile('|'.join(words4),re.IGNORECASE).search(usermessage):
-      print("Do you have any issue in MyAccount or need a new connection? You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue.")
-      flag=0
-      break
-    elif re.compile('|'.join(words5),re.IGNORECASE).search(usermessage):
-      print("Do you have any issue with Payments, Refunds, Energy Bills or want to know about Support Scheme?  You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue.")
-      flag=0
-      break
-    elif re.compile('|'.join(words6),re.IGNORECASE).search(usermessage):
-      print("Do you have any issue with Paying your bills or want to know to reduce energy costs/Maximize household income ?  You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue.")
-      flag=0
-      break
-    elif re.compile('|'.join(words7),re.IGNORECASE).search(usermessage):
-      print("We are sorry to hear this. We'd like to offer our support and help. Although I am just a robot, we can assist you by encouraging you to seek professional help. You are eligible for our priority services. Please visit https://www.ofgem.gov.uk/information-consumers/energy-advice-households/getting-extra-help-priority-services-register to register. These are the resources: https://www.nhs.uk/nhs-services/mental-health-services/ . You can get in touch with them ")
-      break
-    #elif re.compile('|'.join(words8),re.IGNORECASE).search(usermessage):
-      #print("We are aware of your vulnereble circumstance, we'd like to help via these goverment resources. Please visit https://www.nhs.uk/conditions/social-care-and-support-guide/caring-for-children-and-young-people/.")      
-    else:
-        return([usermessage])
-
-
-
-#if __name__ == "__main__":
-  #  chat()
+class Chatbot:
+    def __init__(self):
+        self.message = []
+        self.intro = "Hello, I'm a chatbot. How can I help you?"
+        
+    def chat(self, user_message):
+      while True:
+          words1=["boiler","storage heater","insulation","heat pump","contact someone on my boiler","contact someone on my storage heater","contact someone on my insulation","contact someone on my heat pump"]
+          words2=["faulty thermostat", "damaged thermostat", "display thermostat","HeatSmart", "Netamo smart thermostat"]
+          words3=["electricity supply","gas supply","new connection","new supply","supply disruption","electricity disruption","gas disruption"]
+          words4=["MyAccount", "personal details", "account details", "edf app"]
+          words5=["payment", "payment due","recent payment", "refund", "Energy Bills", "Support Scheme", "Help with the Energy Bills Support Scheme"]
+          words6=["Reduce energy costs", "energy costs", "Maximise household income", "household income", "Paying my bills", "Paying bills", "General debt advice", "debt"]
+          words7=["Illness","disability","mental health issue"]
+          words8=["child", "live with a child", "children", "live alone", "reached state pension age", "live alone or with others and have reached state pension age","disabled",
+          "chronically ill state", "pension age or are disabled","pension age or are disabled", "pensionable age or are disabled","chronically ill or under 18 years old", 
+          "under 18 years old", "medicine refrigerator problem", "medicine refrigerator", "elderly have problem reading their bills","elder", "elderly have problem with hearing on the phone",
+          "elderly have problem with using electronic payments"," child under five years old"]
+          if re.compile('|'.join(words1),re.IGNORECASE).search(user_message):
+             return(["Is your Storage Heater, Boiler, Insulation or Heat Pump not working properly? You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue. If the issue persist you can visit this website https://www.edfenergy.com/heating/boilers/new-boiler."])
+             flag=0
+             break
+          elif re.compile('|'.join(words2),re.IGNORECASE).search(user_message):
+             return(["Is your Smart in-home device, in-home display thermostat or HeatSmart/Netamo smart thermostat Boiler not working properly? You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue."])
+             flag=0
+             break
+          elif re.compile('|'.join(words3),re.IGNORECASE).search(user_message):
+             return(["Do you have an issue with supply disruption or need a new connection? You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue."])
+             flag=0
+             break
+          elif re.compile('|'.join(words4),re.IGNORECASE).search(user_message):
+             return(["Do you have any issue in MyAccount or need a new connection? You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue."])
+             flag=0
+             break
+          elif re.compile('|'.join(words5),re.IGNORECASE).search(user_message):
+             return(["Do you have any issue with Payments, Refunds, Energy Bills or want to know about Support Scheme?  You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue."])
+             flag=0
+             break
+          elif re.compile('|'.join(words6),re.IGNORECASE).search(user_message):
+             return(["Do you have any issue with Paying your bills or want to know to reduce energy costs/Maximize household income ?  You can visit our website https://www.edfenergy.com/for-home/help-support/help-centre to get relevant information on how to fix the issue."])
+             flag=0
+             break
+          elif re.compile('|'.join(words7),re.IGNORECASE).search(user_message):
+             return(["We are sorry to hear this. We'd like to offer our support and help. Although I am just a robot, we can assist you by encouraging you to seek professional help. You are eligible for our priority services. Please visit https://www.ofgem.gov.uk/information-consumers/energy-advice-households/getting-extra-help-priority-services-register to register. These are the resources: https://www.nhs.uk/nhs-services/mental-health-services/ . You can get in touch with them "])
+             break
+            #elif re.compile('|'.join(words8),re.IGNORECASE).search(usermessage):
+              #print("We are aware of your vulnereble circumstance, we'd like to help via these goverment resources. Please visit https://www.nhs.uk/conditions/social-care-and-support-guide/caring-for-children-and-young-people/.")      
+          else:
+             (self.message).append(user_message)
+        
+        
+        
+        #if __name__ == "__main__":
+          #  chat()
